af81becd614c649a596120eec0d88bde21633a06
Caroline Gorham <caroline.gorham@gmail.com>
2023-07-10 23:59:09 -0400

    fix

--- app.py
+++ app.py
@@ -2,6 +2,26 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import smtplib
+import base64
+import io 
+
+def get_table_download_link(df):
+    # Create an in-memory Excel file stream
+    excel_file = io.BytesIO()
+
+    # Save the DataFrame to the Excel file
+    with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False)
+
+    # Seek to the beginning of the stream
+    excel_file.seek(0)
+
+    # Create a base64 encoded string from the Excel data
+    b64 = base64.b64encode(excel_file.read()).decode()
+
+    # Generate a download link with the base64 data
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="table.xlsx">Download XLSX</a>'
+    return href
 
 # Load the data from the Excel file
 data = pd.read_excel('FSSgenAI-usecases-app.xlsx', sheet_name='Sheet1')
@@ -58,6 +78,12 @@ styled_table = filtered_data.style \
 
 # Display the styled table
 st.dataframe(styled_table)
+
+# Add a button to download the table in XLSX format
+if len(filtered_data) > 0:
+    st.sidebar.markdown("<h3 style='text-align: center;'>Download Table</h3>", unsafe_allow_html=True)
+    st.sidebar.markdown(get_table_download_link(filtered_data), unsafe_allow_html=True)
+
 
 # Add a button to send email
 st.sidebar.markdown('Send Email to cgorham@ibm.com and Trinh.Le@ibm.com with additional use case ideas, assets and to designate yourself a primary contact.')
